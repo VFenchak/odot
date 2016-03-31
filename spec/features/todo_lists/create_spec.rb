e20402a7ef0fc97b70e2dc8cfa0bda9e5c6512ec
Sebastian Velandia <sebas.velandia@grupo.ly>
2016-03-31 08:24:50 -0500

    Added method and removed repeated code

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 describe "creating todo_lists" do
-  it "redirects to the index page on sucess" do
+  def create_todo_list(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is my todo list"
+    
     visit '/todo_lists'
     click_link 'New Todo list'
-    fill_in 'Title', with: 'My Todo list'
-    fill_in 'Description', with: 'Some description'
+    
+    fill_in 'Title', with: options[:title]
+    fill_in 'Description', with: options[:description]
     click_button 'Create Todo list'
+  end
+
+  it "redirects to the index page on sucess" do
+    create_todo_list
     expect(page).to have_content 'My Todo list'
   end
   
   it "show one error if title is empty" do
     expect(TodoList.count).to eq(0)
-    visit '/todo_lists'
-    click_link 'New Todo list'
-    fill_in 'Title', with: ''
-    fill_in 'Description', with: 'Some description'
-    click_button 'Create Todo list'
+    create_todo_list title: ''
     expect(page).to have_content 'error'
     expect(TodoList.count).to eq(0)
     visit '/todo_lists'
@@ -25,28 +29,10 @@ describe "creating todo_lists" do
   
   it "title should be > 3" do
     expect(TodoList.count).to eq(0)
-    visit '/todo_lists'
-    click_link 'New Todo list'
-    fill_in 'Title', with: 'HI'
-    fill_in 'Description', with: 'So'
-    click_button 'Create Todo list'
+    create_todo_list title: 'Hi'
     expect(page).to have_content 'error'
     expect(TodoList.count).to eq(0)
     visit '/todo_lists'
     expect(page).to_not have_content 'Some description'
   end
-  # describe "title" do
-#     subject = TodoList.new
-#     subject.title = "HOLA"
-#
-#     it "must be instance of TodoList" do
-#       #@lista.must_be_instance_of TodoList
-#       expect(subject).to be_a TodoList
-#     end
-#
-#     it "title must not be empty" do
-#       #Old syntax -> change it
-#       subject.title.length.should > 0
-#     end
-#   end
 end
