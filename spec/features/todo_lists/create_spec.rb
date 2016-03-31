bf03c2a04dfd766c573e14951a5a42be9d8ba530
Vladyslav Fenchak <vladyslav@aticoestudio.com>
2016-03-31 14:58:25 +0200

    Realizados los test de titulo de ToDo list

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -9,4 +9,44 @@ describe "creating todo_lists" do
     click_button 'Create Todo list'
     expect(page).to have_content 'My Todo list'
   end
+  
+  it "show one error if title is empty" do
+    expect(TodoList.count).to eq(0)
+    visit '/todo_lists'
+    click_link 'New Todo list'
+    fill_in 'Title', with: ''
+    fill_in 'Description', with: 'Some description'
+    click_button 'Create Todo list'
+    expect(page).to have_content 'error'
+    expect(TodoList.count).to eq(0)
+    visit '/todo_lists'
+    expect(page).to_not have_content 'Some description'
+  end
+  
+  it "title should be > 3" do
+    expect(TodoList.count).to eq(0)
+    visit '/todo_lists'
+    click_link 'New Todo list'
+    fill_in 'Title', with: 'HI'
+    fill_in 'Description', with: 'So'
+    click_button 'Create Todo list'
+    expect(page).to have_content 'error'
+    expect(TodoList.count).to eq(0)
+    visit '/todo_lists'
+    expect(page).to_not have_content 'Some description'
+  end
+  # describe "title" do
+#     subject = TodoList.new
+#     subject.title = "HOLA"
+#
+#     it "must be instance of TodoList" do
+#       #@lista.must_be_instance_of TodoList
+#       expect(subject).to be_a TodoList
+#     end
+#
+#     it "title must not be empty" do
+#       #Old syntax -> change it
+#       subject.title.length.should > 0
+#     end
+#   end
 end
