41c9ba9b6e7908884a6b80917e3f3c8105c3b818
Vladyslav Fenchak <vladyslav@aticoestudio.com>
2016-03-31 16:05:08 +0200

    Edit todolist test refactored

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 describe "editing todo list" do
-  it "updates a todo list successfully with correct information" do
-  	todo_list = TodoList.create title: "Groceries", "Grocery list."
-
-  	visit "/todo_lists"
+  def update_todo_list(options={})
+    options[:title] ||= "New title"
+    options[:description] ||= "New description"
+    todo_list = options[:todo_list]
+    visit '/todo_lists'
   	within "#todo_list_#{todo_list.id}" do
   		click_link "Edit"
   	end
-
-  	fill_in "Title", with: "New title"
-  	fill_in "Description", with: "New description"
-  	click_button "Update Todo list"
-
+    
+    fill_in 'Title', with: options[:title]
+    fill_in 'Description', with: options[:description]
+    click_button 'Update Todo list'
+  end
+  
+  it "updates a todo list successfully with correct information" do
+  	todo_list = TodoList.create title: "Groceries", description: "Grocery list."
+    update_todo_list todo_list: todo_list
+  	
+    #recargamos la variable
+    todo_list.reload
   	expect(page).to have_content("Todo list was successfully updated")
   	expect(todo_list.title).to eq("New title")
   	expect(todo_list.description).to eq("New description")
