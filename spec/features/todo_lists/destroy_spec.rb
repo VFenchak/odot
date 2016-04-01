b4c6196f1da6f356b93f1ba14c19fcc25106fb70
Sebastian Velandia <sebas.velandia@grupo.ly>
2016-04-01 07:22:03 -0500

    Fixed destoy spec test case

--- spec/features/todo_lists/destroy_spec.rb
+++ spec/features/todo_lists/destroy_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe "destroying todo list" do
 	let!(:todo_list) { TodoList.create title: "Groceries", description: "Grocery list." }
 
-	it "destroy the todo list correctly", js: true  do
+	it "Destroys successfully when clicking the destroy link"  do
 		visit '/todo_lists'
 		within "#todo_list_#{todo_list.id}" do
   			click_link "Destroy"
-  			click_button('Accept')
   		end
-  		expect(page).to have_content("Groceries")
+  		expect(page).to_not have_content("Groceries")
+  		expect(TodoList.count).to eq(0)
 	end
 end
